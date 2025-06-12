c092c30a3ce9e58782a22555393f1879d63422c9
tonygrimstad <65394379+tonygrimstad@users.noreply.github.com>
2025-06-12 23:12:38 +0200

    fslightbox

--- src/data/galleriIndex.ts
+++ src/data/galleriIndex.ts
@@ -1,4 +1,5 @@
+import tm2019 from "./gallery/TM2019-Gallery.json";
 import tm2022 from "./gallery/TM2022-Gallery.json";
 import tm2023 from "./gallery/TM2023-Gallery.json";
 
-export const galleriData = [tm2023, tm2022]; // rekkefølge = visningsrekkefølge
+export const galleriData = [tm2019, tm2022, tm2023]; // rekkefølge = visningsrekkefølge
---
+++ src/types/global.d.ts
@@ -0,0 +1,7 @@
+declare global {
+  interface Window {
+    bindImageEvents?: () => void;
+  }
+}
+
+export {};
