92d505c36cb1ffa5e7852515f70f589b1157c4b4
tonygrimstad <65394379+tonygrimstad@users.noreply.github.com>
2025-08-02 20:00:57 +0200

    photo 2025

--- src/data/galleriIndex.ts
+++ src/data/galleriIndex.ts
@@ -5,5 +5,6 @@ import tm2018 from "./gallery/TM2018-Gallery.json";
 import tm2019 from "./gallery/TM2019-Gallery.json";
 import tm2022 from "./gallery/TM2022-Gallery.json";
 import tm2023 from "./gallery/TM2023-Gallery.json";
+import tm2025 from "./gallery/TM2025-Gallery.json";
 
-export const galleriData = [tm2015, tm2016, tm2017, tm2018, tm2019, tm2022, tm2023]; // rekkefølge = visningsrekkefølge
+export const galleriData = [tm2015, tm2016, tm2017, tm2018, tm2019, tm2022, tm2023, tm2025]; // rekkefølge = visningsrekkefølge
